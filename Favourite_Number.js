6dc95042ba7427ffd4f4d0ad00838d9f30800153
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-11 05:06:06 +0500

    Add files via upload

---
+++ Favourite_Number.js
@@ -0,0 +1,5 @@
+"use strict";
+let favorite_number = 7;
+let message = 'here is your favorite number' + (favorite_number);
+console.log(`${favorite_number}`);
+console.log(`${message}`);
---
+++ Favourite_Number.ts
@@ -0,0 +1,5 @@
+let favorite_number :number = 7
+let message : string = 'here is your favorite number' + (favorite_number)
+
+console.log(`${favorite_number}`);
+console.log(`${message}`);
